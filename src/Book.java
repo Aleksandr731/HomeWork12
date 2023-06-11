fa3f4f649aab3d297f6f15cfad7b704863231e63
Александр <alexst731w@gmail.com>
2023-06-11 18:38:39 +0300

    ДЗ-12

--- src/Book.java
+++ src/Book.java
@@ -1,24 +1,14 @@
 public class Book {
-    private String author;
+    private Author author;
     private String bookName;
     int publishingYear;
 
-
-    public Book(String bookName, int publishingYear, String author) {
-
-        this.bookName = bookName;
-
-        this.publishingYear = publishingYear;
-
-        this.author = author;
-
-    }
-    public Book(String author, String bookName, int publishingYear) {
+    public Book(Author author, String bookName, int publishingYear) {
         this.author = author;
         this.bookName = bookName;
         this.publishingYear = publishingYear;
     }
-    public String getAuthor() { return this.author; }
+    public Author getAuthor() { return this.author; }
     public String getBookName() { return this.bookName; }
     public int getPublishingYear() { return this.publishingYear; }
 
